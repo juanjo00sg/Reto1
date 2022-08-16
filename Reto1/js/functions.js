e48530c2610c3a4888845fc9a20fa492cc12b5c2
juanjo00sg <jujo00sg@gmail.com>
2022-08-16 17:26:11 -0500

    JS functions for validations on Reto1

---
+++ Reto1/js/functions.js
@@ -0,0 +1,61 @@
+function main() {
+  let classes = initializeClasses();
+  let ans;
+  do {
+    ans = findCourses(classes);
+  } while (checkHours(ans));
+  console.log(ans)
+}
+/* Funtion that returns the Class object */
+function createClass(name, hours) {
+  return {
+    name: name,
+    hours: hours,
+  };
+}
+
+/* Code with initial info about classes and courses */
+function initializeClasses() {
+  return [
+    10,
+    createClass("Inteligencia Artificial", 4),
+    createClass("Investigación de Procesos", 2),
+    createClass("Dispositivos Móviles", 4),
+    createClass("Proyecto de Desarrollo", 3),
+  ];
+}
+
+function checkHours(array) {
+  classes = structuredClone(array);
+
+  let maxVal = classes.shift();
+  let i;
+  for (const c in classes) {
+    i =+ c.hours;
+  }
+  if (i <= maxVal) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+/* Checks for the max hours in the array and returns them */
+function findCourses(array) {
+  classes = structuredClone(array);
+
+  let maxVal = classes.shift();
+
+  let maxClass1 = Math.max(...classes.map((o) => o.hours));
+  let obj1 = classes.find((element) => (element.hours = maxClass1));
+  let index1 = classes.indexOf(obj1);
+  classes.splice(index1, 1);
+
+  let maxClass2 = Math.max(...classes.map((o) => o.hours));
+  let obj2 = classes.find((element) => (element.hours = maxClass2));
+  let index2 = classes.indexOf(obj1);
+  classes.splice(index2, 1);
+
+  return [obj1, obj2];
+}
